b04573fa11250c465f21044980c8d2b6264564e0
Paulo Coutinho <paulo@prsolucoes.com>
2019-04-12 20:01:56 -0300

    new logic to get target configuration

--- make.py
+++ make.py
@@ -8,7 +8,5 @@ from modules import ezored
 ezored_path = os.path.dirname(os.path.abspath(__file__))
 proj_path = ezored_path
 
-proj_path = '/Users/paulo/Developer/workspaces/cpp/ubook-sdk-mobile'
-
 if __name__ == '__main__':
     ezored.run(ezored_path, proj_path, sys.argv)
